906def9c7c762fa56022e244c44f61cd7499d6c9
woaigjh <wo94aigjh@gmail.com>
2025-03-12 18:40:56 +0800

    吃饭

--- server.js
+++ server.js
@@ -137,6 +137,11 @@ app.post('/api/chat', async (req, res) => {
         body: req.body
     });
 
+    // 设置CORS响应头
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT');
+    res.setHeader('Access-Control-Allow-Headers', 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, Authorization');
+
     // 处理OPTIONS请求
     if (req.method === 'OPTIONS') {
         return res.status(200).end();
